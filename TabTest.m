a35d67c7e7c25ff0c355aa1ae91e19bf2d1754a1
omerishaq <omer.ishaq@gmail.com>
2015-02-04 14:46:23 +0100

    Results File in Tab 1
    - Changed the code to make sure that the Results.mat file is being
    created in Tab 1 rather than being created in a separate script.

--- TabTest.m
+++ TabTest.m
@@ -13,6 +13,7 @@ close all
 clc
 
 % perform all initializations in this block and create the GUIs
+create_ResultsFile();
 initialize_Application();
 
 create_Model();
@@ -22,6 +23,23 @@ settings = initialize_Settings();       % initialize the settings
 hTabGroup = create_GUI(settings);       % create the GUI
 initialize_GUI(hTabGroup);              % initialize the GUI
 initialize_LookAndFeel();               % initialize the look and feel
+
+end
+
+function [] = create_ResultsFile ()
+
+str_filename = 'Results.mat';
+
+    if exist(str_filename, 'file')
+        % Then do nothing if it already exists
+    else
+        Records.peak    = 0;
+        Records.r       = 0;
+        Records.c       = 0;
+        Records.user    = 'test user';
+        Records.img     = 'test user';
+        save Results.mat Records
+    end
 
 end
 
