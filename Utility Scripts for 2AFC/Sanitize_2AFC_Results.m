89ba1ae06d087c1403309560c37874954e5d189e
omerishaq <omer.ishaq@gmail.com>
2015-03-31 08:57:10 +0200

    Created new script for data sanitization
    - added a utility folder and a data sanitization script

---
+++ Utility Scripts for 2AFC/Sanitize_2AFC_Results.m
@@ -0,0 +1,33 @@
+% Script created for the ML-2015 project
+% Created by Omer Ishaq. Copy right @ omer.ishaq@gmail.com
+% Input:
+%
+% Output:
+%
+% Action: The script is a utility script written to assist the 2AFC tool, it does the following
+%
+% 1. Finds data for a particular image.
+% 2. Finds and sorts the results into fg and bg classes.
+%
+
+clearvars
+clear global % for removing global variables
+close all
+clc
+
+% set the required parameters below.
+data_folder = '';
+results_folder = '';
+image_name = '';
+fg_folder = '';
+bg_folder = '';
+
+% read the data folder
+% read the spot data aaccording to the image_name
+% read the corresponding results folder
+% overlay data
+% threshold into fg and bg
+% put in two other folders
+
+
+
